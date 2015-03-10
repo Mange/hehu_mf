728361b5e403cd2ec6988c14fd3f0aad9d796c78
Magnus Bergmark <magnus.bergmark@gmail.com>
2015-03-10 21:18:23 +0100

    Remove inventory from Arsenal Box

--- config.cpp
+++ config.cpp
@@ -49,8 +49,7 @@ class CfgVehicles {
 		};
 	};
 
-	class ReammoBox_F;
-	class Box_NATO_AmmoVeh_F;
+	class thingX;
 
 	class HEHU_CQB_Module: Module_F
 	{
@@ -217,8 +216,24 @@ class CfgVehicles {
 		};	
 	};
 
-	class HEHU_CQB_ArsenalBox: Box_NATO_AmmoVeh_F
+	class HEHU_CQB_ArsenalBox: thingX
 	{
+		// Don't inherit from a real ammo box to remove the vanilla inventory from them.
+		// You can only interact with this box as a Virtual Arsenal object.
+		//
+		// class thingX;
+		//   class ReammoBox_F;
+		//      class Box_NATO_Wps_F;
+		//
+		// TODO: Is it possible to sling load this box? What would be required to make that work?
+
+		vehicleclass = "Ammo";  // ReammoBox_F
+		animated = 0;           // ReammoBox_F
+		accuracy = 0.2;         // ReammoBox_F
+		icon = "iconCrateWpns"; // Box_NATO_Wps_F
+		mapsize = 1.81;         // Box_NATO_Wps_F
+		model = "\A3\weapons_F\AmmoBoxes\WpnsBox_F"; // Box_NATO_Wps_F
+
 		displayName = "HEHU Ammo box";
 		author = "Magnus Bergmark";
 		scope = 2;
