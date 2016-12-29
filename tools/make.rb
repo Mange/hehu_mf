e862bd6ac7f38c59aaa5b3e61c7e7f7667416e2b
Magnus Bergmark <magnus.bergmark@gmail.com>
2016-12-29 21:50:50 +0000

    Improve make tool
    * Show command that is being run
    * Add a release argument to place in a `@hehu` folder
    * Actually skip the "blank" module

--- tools/make.rb
+++ tools/make.rb
@@ -1,16 +1,27 @@
+require "shellwords"
 require "pathname"
-source_path = File.expand_path("../..", __FILE__)
+source_path = Pathname.new(File.expand_path("../..", __FILE__))
+
+if ARGV.include?("release")
+	out_dir = source_path.join("release", "@hehu", "addons")
+else
+	out_dir = source_path.join("addons")
+end
+
+out_dir.mkpath
 
 Pathname.glob("#{source_path}/addons/*").select(&:directory?).each do |dir|
-	next if dir.basename == "blank"
+	next if dir.basename.to_s == "blank"
 
-	unless system(
+	command = [
 		"makepbo",
 		"-NUP",
-		#"-@=z\\hehu\\addons\\#{dir.basename}",
 		dir.to_s,
-		"#{dir.dirname}/hehu_#{dir.basename}.pbo"
-	)
+		out_dir.join("hehu_#{dir.basename}.pbo").to_s
+	].compact
+
+	puts command.map(&:shellescape).join(" ")
+	unless system(*command)
 		puts "PBO failed! Press enter to close window."
 		gets
 		exit 1
