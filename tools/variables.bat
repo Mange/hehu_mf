d3d72c800f682788bb507aa2cd833ce42c37b169
Magnus Bergmark <magnus.bergmark@gmail.com>
2016-12-29 19:55:21 +0000

    New computer, new paths

--- tools/variables.bat
+++ tools/variables.bat
@@ -2,10 +2,10 @@ rem Important: Don't use quotes when setting variables, even when the filename
 rem contains spaces!
 
 rem Directory where the Arma 3 Tools from BIS is installed.
-set tools_dir=P:\
+set tools_dir=D:\SteamLibrary\SteamApps\common\Arma 3 Tools
 
 rem Path of the mod
 set source_dir=%~dp0\..
 
 rem Path to installed Arma 3
-set arma3_dir=C:\Program Files (x86)\Steam\steamApps\common\Arma 3
+set arma3_dir=C:\SteamLibrary\steamApps\common\Arma 3
